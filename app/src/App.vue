0005eb36fb2c75c667eaf2ee4f5792660da5fe1e
Steven Soule <me@stevensoule.com>
2019-09-16 23:21:11 -0700

    copy-paste Map compnent working | needs refactor

--- app/src/App.vue
+++ app/src/App.vue
@@ -10,9 +10,24 @@
 <script>
 import Header from '@/components/Header.vue'
 
+// import api from '@/api.js'
+// import store from '@/store.js'
+
 export default {
   components: {
     Header
-  }
+	},
+	/* beforeCreate() {
+		this.$store.commit('initialiseStore');
+	} */
+  /* beforeMount: function () {
+    this.load_compared_cities();
+  },
+  methods: {
+    load_compared_cities: function () {
+			console.log('load_compared_cities', store.state.compared_city_ids);
+			
+    }
+  }, */
 }
 </script>
--- app/src/store.js
+++ app/src/store.js
@@ -3,12 +3,26 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import {version} from '../package.json';
+
 export default new Vuex.Store({
   state: {
-
+    version
   },
   mutations: {
-
+    initialiseStore(state) {
+      // Check if the store exists
+      if(localStorage.getItem('store')) {
+        let store = JSON.parse(localStorage.getItem('store'));
+        
+        if(store.version == version) {
+          this.replaceState(
+            Object.assign(state, store)
+          )
+        }
+      }
+      localStorage.setItem('store', JSON.stringify(state))
+    }
   },
   actions: {
 
